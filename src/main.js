a0441a475d44e92812732b70decda3bcca4401e6
hacker <157203458+hacker-frok@users.noreply.github.com>
2024-01-31 12:59:49 +0800

    优化性能，去掉一些文件监听

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
-const { BrowserWindow, ipcMain ,webContents} = require('electron')
+const { BrowserWindow, ipcMain, webContents } = require('electron')
 const fs = require('fs')
 const path = require('path')
 
-let userInfo=undefined
+let userInfo = undefined
 
 function log(...args) {
     console.log(`\x1b[35m[QQ怀旧模式]\x1b[0m`, ...args);
 }
 let mainWindow
-function updateStyle(webContent, settingsPath,winStyle) {
+const pluginDataPath = LiteLoader.plugins["nostalgic"].path.data;
+const settingsPath = path.join(pluginDataPath, "settings.json");
+
+function updateStyle(winStyle) {
 
     try {
         // 读取settings.json
@@ -18,10 +21,10 @@ function updateStyle(webContent, settingsPath,winStyle) {
         const themeColor = config.themeColor;
         const themeColor2 = config.themeColor2;
         const themeColor3 = config.themeColor3;
-        const textOpacity = config.backgroundOpacity/100;
+        const textOpacity = config.backgroundOpacity / 100;
         const textOpacityHex = Math.round(config.backgroundOpacity * 2.55).toString(16).padStart(2, "0");
         let csspath = path.join(__dirname, "/settings/style.css");
-        if(winStyle=='Big'){
+        if (winStyle == 'Big') {
             csspath = path.join(__dirname, "/settings/styleBig.css");
         }
         fs.readFile(csspath, "utf-8", (err, data) => {
@@ -44,13 +47,13 @@ function updateStyle(webContent, settingsPath,winStyle) {
 
             }
             //向渲染进程发送updateStyle消息
-            if(mainWindow){
+            if (mainWindow) {
                 mainWindow.webContents.send(
                     "LiteLoader.nostalgic.updateStyle",
                     preloadString + "\n\n" + data
                 );
-            }else{
-             webContents.getAllWebContents().forEach((webContent) => {
+            } else {
+                webContents.getAllWebContents().forEach((webContent) => {
                     webContent.send(
                         "LiteLoader.nostalgic.updateStyle",
                         preloadString + "\n\n" + data
@@ -58,15 +61,12 @@ function updateStyle(webContent, settingsPath,winStyle) {
                 });
             }
 
-         });
+        });
     } catch (error) {
         log(error.message)
     }
 
 }
-// 加载插件时触发
-const pluginDataPath = LiteLoader.plugins["nostalgic"].path.data;
-const settingsPath = path.join(pluginDataPath, "settings.json");
 
 // fs判断插件路径是否存在，如果不存在则创建（同时创建父目录（如果不存在的话））
 if (!fs.existsSync(pluginDataPath)) {
@@ -77,13 +77,13 @@ if (!fs.existsSync(settingsPath)) {
     fs.writeFileSync(settingsPath, JSON.stringify({
         "useOldTheme": true,
         "themeColor": "#21d4fd",
-        "themeColor2":"#3316c5",
-        "themeColor3":"#ffffff",
+        "themeColor2": "#3316c5",
+        "themeColor3": "#ffffff",
         "backgroundOpacity": "85",
-        "isDebug":false,
-        "useOldThemeWin":true,
-        "hideSwitchBtn":true,
-        "useOldThemeMegList":false
+        "isDebug": false,
+        "useOldThemeWin": true,
+        "hideSwitchBtn": true,
+        "useOldThemeMegList": false
     }));
 } else {
 
@@ -94,7 +94,7 @@ if (!fs.existsSync(settingsPath)) {
         fs.writeFileSync(settingsPath, JSON.stringify(config));
     }
     if (!config.themeColor) {
-        config.themeColor =  "#21d4fd";
+        config.themeColor = "#21d4fd";
         fs.writeFileSync(settingsPath, JSON.stringify(config));
     }
     if (!config.themeColor2) {
@@ -110,20 +110,20 @@ if (!fs.existsSync(settingsPath)) {
         fs.writeFileSync(settingsPath, JSON.stringify(config));
     }
 
-    if (config.useOldThemeWin==undefined||config.useOldThemeWin==null) {
+    if (config.useOldThemeWin == undefined || config.useOldThemeWin == null) {
         config.useOldThemeWin = true;
         fs.writeFileSync(settingsPath, JSON.stringify(config));
     }
-    if (config.useOldThemeMegList==undefined||config.useOldThemeMegList==null) {
+    if (config.useOldThemeMegList == undefined || config.useOldThemeMegList == null) {
         config.useOldThemeMegList = true;
         fs.writeFileSync(settingsPath, JSON.stringify(config));
     }
 
-    if (config.hideSwitchBtn==undefined||config.hideSwitchBtn==null) {
+    if (config.hideSwitchBtn == undefined || config.hideSwitchBtn == null) {
         config.hideSwitchBtn = true;
         fs.writeFileSync(settingsPath, JSON.stringify(config));
     }
-     if (!config.isDebug) {
+    if (!config.isDebug) {
         config.isDebug = false;
         fs.writeFileSync(settingsPath, JSON.stringify(config));
     }
@@ -134,7 +134,7 @@ ipcMain.on(
     "LiteLoader.nostalgic.rendererReady",
     (event, message) => {
         const window = BrowserWindow.fromWebContents(event.sender);
-        updateStyle(window.webContents, settingsPath);
+        updateStyle();
     }
 );
 
@@ -144,24 +144,9 @@ ipcMain.on(
     (event, winStyle) => {
         const window = BrowserWindow.fromWebContents(event.sender);
         const settingsPath = path.join(pluginDataPath, "settings.json");
-        updateStyle(window.webContents, settingsPath,winStyle);
+        updateStyle(winStyle);
     });
 
-// 监听渲染进程的watchCSSChange事件
-ipcMain.on(
-    "LiteLoader.nostalgic.watchCSSChange",
-    (event, settingsPath) => {
-        const window = BrowserWindow.fromWebContents(event.sender);
-        watchCSSChange(window.webContents, settingsPath);
-    });
-
-// 监听渲染进程的watchSettingsChange事件
-ipcMain.on(
-    "LiteLoader.nostalgic.watchSettingsChange",
-    (event, settingsPath) => {
-        const window = BrowserWindow.fromWebContents(event.sender);
-        watchSettingsChange(window.webContents, settingsPath);
-    });
 
 ipcMain.handle(
     "LiteLoader.nostalgic.getSettings",
@@ -184,6 +169,7 @@ ipcMain.handle(
         try {
             const new_config = JSON.stringify(content);
             fs.writeFileSync(settingsPath, new_config, "utf-8");
+            updateStyle();
         } catch (error) {
             log(error);
         }
@@ -203,48 +189,17 @@ ipcMain.handle(
     }
 );
 
-// 防抖函数
-function debounce(fn, time) {
-    let timer = null;
-    return function (...args) {
-        timer && clearTimeout(timer);
-        timer = setTimeout(() => {
-            fn.apply(this, args);
-        }, time);
-    }
-}
-//监听文件修改（开发时使用）
-function watchCSSChange(webContents, settingsPath) {
-    const filepath = path.join(__dirname, "/settings/style.css");
-    watcherStyleFile= fs.watchFile(filepath,{interval: 20}, debounce(() => {
-        updateStyle(webContents, settingsPath);
-    }, 100));
-}
 
-ipcMain.handle(
-    "LiteLoader.nostalgic.setDebug",
-    (event, open) => {
-        const window = BrowserWindow.fromWebContents(event.sender);
-        const filepath = path.join(__dirname, "/settings/style.css");
-        open?watchCSSChange(window.webContents, settingsPath):(fs.unwatchFile(filepath),null);
-});
-
-// 监听配置文件修改
-function watchSettingsChange(webContents, settingsPath) {
-    fs.watch(settingsPath, "utf-8", debounce(() => {
-        updateStyle(webContents, settingsPath);
-    }, 100));
-}
 
 // 创建窗口时触发
 module.exports.onBrowserWindowCreated = window => {
 
     const original_send = window.webContents.send;
     const patched_send = (channel, ...args) => {
-        if(JSON.stringify(args).includes('nodeIKernelProfileListener/onProfileDetailInfoChanged')){
+        if (JSON.stringify(args).includes('nodeIKernelProfileListener/onProfileDetailInfoChanged')) {
             //console.log(args?.[1]?.[0]?.payload.info,JSON.stringify(args?.[1]?.[0]?.payload))
-            userInfo=args?.[1]?.[0]?.payload.info
-           // log("用户信息更新",userInfo)
+            userInfo = args?.[1]?.[0]?.payload.info
+            // log("用户信息更新",userInfo)
         }
         return original_send.call(window.webContents, channel, ...args);
     };
@@ -253,22 +208,12 @@ module.exports.onBrowserWindowCreated = window => {
         if (window.webContents.getURL().indexOf("#/main/message") !== -1) {
 
             mainWindow = window;
-             
+
             mainWindow.setMinimumSize(280, 600);
             //  mainWindow.setMaximumSize(400, 9999);
             // mainWindow.setResizable(false);
         }
     });
-    const settingsPath = path.join(pluginDataPath, "settings.json");
-    window.on("ready-to-show", () => {
-        const url = window.webContents.getURL();
-        if (url.includes("app://./renderer/index.html")) {
-            const data = fs.readFileSync(settingsPath, "utf-8");
-            const config = JSON.parse(data);
 
-            config.isDebug && watchCSSChange(window.webContents, settingsPath);
-            watchSettingsChange(window.webContents, settingsPath);
-        }
-    });
 
 }
